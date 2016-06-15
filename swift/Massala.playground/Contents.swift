a35746cb1ca37946a7d31ea0fcb5ff97bfd39178
Elvis Perazic <bachelor2@iMacdeAutres2.fro-206.froment.in.iesanetwork.com>
2016-06-15 14:24:07 +0200

    #2 constantes et variables

---
+++ swift/Massala.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playgrorund"
+
+print("hello elvis what's up")
+
+// 2
+
+var myVariable = 42
+
+myVariable = 50
+
+var myOtherVariable :Double
+
+
+// 3 
+
+let implicitInteger = 70
+
+let implicitDouble = 70
+
+let explicitDouble :Double = 70
+
+
