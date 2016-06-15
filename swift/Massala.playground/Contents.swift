afbe36197747173d4150015775d81518ebfa84b7
Elvis Perazic <bachelor2@iMacdeAutres2.fro-206.froment.in.iesanetwork.com>
2016-06-15 14:38:51 +0200

    #2 string

--- swift/Massala.playground/Contents.swift
+++ swift/Massala.playground/Contents.swift
@@ -23,4 +23,20 @@ let implicitDouble = 70
 
 let explicitDouble :Double = 70
 
+// 4 "4", toInt() !
 
+let label = "The width is"
+
+let width = 94
+
+let widthLabel = label + String(width)
+
+// 5
+
+let apples = 3
+
+let oranges = 5
+
+let appleSummary = "I have \(apples) apples."
+
+let fruits = "I have \(apples + oranges) fruits"
