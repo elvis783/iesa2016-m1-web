2b94ebedb4c57a220cb44170277a3b19d6790ced
Mastere 1 IT <mastere1it@iMacdeAutres60.sto-002.saint-augustin.in.iesanetwork.com>
2016-06-20 11:10:10 +0200

    #1 Constructeur

--- swift/Swift.playground/Contents.swift
+++ swift/Swift.playground/Contents.swift
@@ -3,3 +3,37 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+// 23 
+
+class Shape {
+    var sidesCount = 0
+    func getInfo() -> String{
+        return "A shape \(sidesCount) sides."
+    }
+}
+
+var shape = Shape()
+
+
+// 24 
+
+/*
+var shape = Shape()
+shape.sidesCount: Int = 0
+var name : String
+init(name: String) { self.name = name }
+func getInfo() -> String { return }
+*/
+
+
+// 25 
+
+    class NamedShape {
+        var sidesCount: Int = 0
+        var name: String
+        init(name: String) { self.name = name }
+        func getInfo() -> String { return "\(sidesCount) sides"}
+
+var carre = NamedShape( name: "drapeau");
